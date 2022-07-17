8be7e1ebbfda3ef2147a75a8e55e21fc333fd334
Karol <karol.wychowaniak@gmail.com>
2022-07-17 10:37:25 +0200

    initial commit

---
+++ job-scrapper/src/test/java/pl/sda/JobScrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobScrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2758b3a0132edb91bef50578dd30a755eff73a0e
Karol <karol.wychowaniak@gmail.com>
2022-07-24 14:57:18 +0200

    added salary filter - still nullPointerException when min salary in view is empty

--- src/main/java/pl/sda/repository/impl/NoFluffJobsJobScrapper.java
+++ src/main/java/pl/sda/repository/impl/NoFluffJobsJobScrapper.java
@@ -10,6 +10,8 @@ import pl.sda.repository.JobScrapper;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 
@@ -26,15 +28,19 @@ public class NoFluffJobsJobScrapper implements JobScrapper {
                 for (Element jobOffer : document.getElementsByClass("posting-list-item")) {
 
                     String title = jobOffer.getElementsByTag("h3").text();
-//                String company = jobOffer.getElementsByClass("d-block").text();
+                    String company = jobOffer.getElementsByAttributeValue("data-cy", "company name on the job offer listing").text();
                     String location = jobOffer.getElementsByClass("mr-1").text();
                     if(location.isEmpty()){
                         location = "Zdalna";
                     }
-                    String company = jobOffer.getElementsByAttributeValue("data-cy", "company name on the job offer listing").text();
-                    String salary = jobOffer.getElementsByAttributeValue("data-cy", "salary ranges on the job offer listing").text();
+                    String salary = jobOffer.getElementsByAttributeValue("data-cy", "salary ranges on the job offer listing").html().toString().replaceAll("&nbsp;","").trim();
+//                    String salary = jobOffer.getElementsByAttributeValue("data-cy", "salary ranges on the job offer listing").text();
                     String link = jobOffer.attr("href");
-                    offers.add(new JobOffer(title, company, location, salary, link));
+
+//                    JobOffer.SalaryRange salaryRange = new JobOffer.SalaryRange();
+                    JobOffer.SalaryRange salaryRange = populateSalaryRange(salaryList(salary));
+
+                    offers.add(new JobOffer(title, company, location, salaryRange, link));
                 }
                 thereIsNextPage = checkIfNextPageExists(document);
             } catch (IOException e) {
@@ -56,4 +62,33 @@ public class NoFluffJobsJobScrapper implements JobScrapper {
         }
         return false;
     }
+
+    private List<Double> salaryList(String salary){
+        List<String> result = null;
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(salary);
+        if(m.find()) {
+            result = new ArrayList<String>();
+            result.add(m.group());
+            while (m.find()){
+                result.add(m.group());
+            }
+        }
+        return result.stream().map(e-> Double.valueOf(e)).collect(Collectors.toList());
+    }
+
+    private JobOffer.SalaryRange populateSalaryRange(List<Double> salaryList){
+        JobOffer.SalaryRange salaryRange = new JobOffer.SalaryRange();
+        if(salaryList.isEmpty()){
+            salaryRange = new JobOffer.SalaryRange(0);
+        }else if(salaryList.size() == 1){
+            salaryRange = new JobOffer.SalaryRange(salaryList.get(0));
+        }else if(salaryList.size() == 2){
+            salaryRange = new JobOffer.SalaryRange(salaryList.get(0), salaryList.get(1));
+        }
+        return salaryRange;
+
+    }
+
+    // metoda przerabiająca salary na [] int
 }

b76772f911e84ecd7c19af96566bb5b4f904aeeb
Karol <karol.wychowaniak@gmail.com>
2022-07-29 16:06:06 +0200

    - added heroku java 17 configuration

--- src/main/java/pl/sda/jobScrapper/JustJoinItJobScrapper.java
+++ src/main/java/pl/sda/jobScrapper/JustJoinItJobScrapper.java
@@ -17,6 +17,9 @@ public class JustJoinItJobScrapper implements JobScrapper {
 
     // tutaj będzie ciężej bo chyba musze wejść do każdej oferty, nie da się wyciągnąć z listy ogłoszeń
     // albo nie:
-    // class="jss239" - title
+    // class="jss233" - title
+    // class="jss250 jss1249" - salary moze byc Undisclosed Salary
+    // class="jss239" - company
+    // class="jss240" - location ew 235 Fully Remote
     // pomyśleć czy do nazwy firmy chcę się bawić we wchodzenie do każdego ogłoszenia
 }
